586e205793696a87dbc98d856e9db490fc61e30c
sanju gajjar <53038038+sanju-gajjar@users.noreply.github.com>
2021-09-23 13:11:47 +0530

    Update minifi.js
    new updated

--- utils/minifi.js
+++ utils/minifi.js
@@ -4,23 +4,38 @@ var decode = require('unescape');
 const replaceString = require('replace-string');
 var beautifyJson = require("json-beautify");
 var beautify = require('js-beautify').js;
-var UglifyJS = require("uglify-js");
 
 exports.minify = function (textToMinify) {
-
-    var result = minify(textToMinify, {
+    var result = minify(removeSpc(textToMinify), {
         removeAttributeQuotes: true
     });
     return result;
 }
 
-exports.beautify = function (textToBeautify) {
-    var beauty = pretty(textToBeautify);
+exports.beautify = function (cleanSpl) {
+    var beauty = pretty(removeSpc(cleanSpl));
     return beauty;
 }
 
 exports.removeSpl = function (cleanSpl) {
+    return removeSpc(cleanSpl);
+}
+exports.beautifyJSON = function (textToBeautify) {
+    //  console.log(JSON.parse(textToBeautify));
 
+    var clean = removeSpc(textToBeautify)
+    var beauty = beautify(clean, { indent_size: 2, space_in_empty_paren: true });
+
+    return beauty;
+}
+exports.beautifyJS = function (textToBeautify) {
+    //  console.log(JSON.parse(textToBeautify));
+    var beauty = beautify(removeSpc(textToBeautify), { indent_size: 2, space_in_empty_paren: true });
+
+    return beauty;
+}
+
+function removeSpc(cleanSpl) {
     cleanSpl = replaceString(cleanSpl, '&amp;', ' ');
     cleanSpl = replaceString(cleanSpl, 'amp;', ' ');
     cleanSpl = replaceString(cleanSpl, '&gt;', '>');
@@ -30,31 +45,12 @@ exports.removeSpl = function (cleanSpl) {
     cleanSpl = replaceString(cleanSpl, 'lt;', '<');
     cleanSpl = replaceString(cleanSpl, 'amp;lt;', '<');
     cleanSpl = replaceString(cleanSpl, '&#xD;', ' ');
-    cleanSpl = replaceString(cleanSpl, '\"', '"');
     cleanSpl = replaceString(cleanSpl, '\n', ' ');
+    cleanSpl = replaceString(cleanSpl, '\\n', ' ');
+    cleanSpl = replaceString(cleanSpl, '\\', '');
+    cleanSpl = replaceString(cleanSpl, '\"', '"');
+    cleanSpl = replaceString(cleanSpl, '\\"', '"');
+
     result = decode(cleanSpl, 'all');
     return result;
-}
-exports.beautifyJSON = function (textToBeautify) {
-    //  console.log(JSON.parse(textToBeautify));
-    var beauty = beautify(textToBeautify, { indent_size: 2, space_in_empty_paren: true });
-
-    return beauty;
-}
-exports.beautifyJS = function (textToBeautify) {
-    //  console.log(JSON.parse(textToBeautify));
-    var beauty = beautify(textToBeautify, { indent_size: 2, space_in_empty_paren: true });
-
-    return beauty;
-}
-exports.minifyJS = function (textToMinify) {
-    var beauty = UglifyJS.minify(textToMinify, {
-        keep_fnames: true,
-        mangle: false,
-        compress: false
-    });
-    if (beauty.error) {
-        return JSON.stringify(beauty.error.message + " AT : line:" + beauty.error.line + " col:" + beauty.error.col + " pos:" + beauty.error.pos);
-    }
-    return beauty.code;
 }
